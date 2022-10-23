90beb9987f11abcb44178ce5e3abf25e45ae3a62
Milo-Shen <Milo-Shen@outlook.com>
2022-10-23 11:15:47 +0800

    feat: init The-Rustonomicon

---
+++ src/chapter_1.rs
@@ -0,0 +1,22 @@
+// https://learnku.com/docs/nomicon/2018/21-repr/4708
+
+use std::mem;
+
+struct A {
+    a: u8,
+    b: u32,
+    c: u16,
+}
+
+struct B {
+    a: u8,
+    b: u32,
+    c: u16,
+}
+
+pub fn repr() {
+    let a = A { a: 1, b: 1, c: 1 };
+    println!("struct A size = {}", mem::size_of::<A>());
+    println!("struct B size = {}", mem::size_of::<B>());
+    println!("size of variable a = {}", mem::size_of_val(&a));
+}
---
+++ src/main.rs
@@ -0,0 +1,5 @@
+mod chapter_1;
+
+fn main() {
+    chapter_1::repr();
+}
